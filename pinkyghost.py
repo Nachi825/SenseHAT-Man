a6ab694671c4d10e4e5f6978f85d486531fb69ce
Nachi Meguro <20048527@tafe.wa.edu.au>
2020-08-24 17:26:41 +0800

    pinkyghost.py

---
+++ pinkyghost.py
@@ -0,0 +1,52 @@
+"""
+    Ghost: Blinky
+
+    This file defines the frames for Blinky
+
+"""
+
+from sensible_colours import *
+
+pinky_frames = [
+    [
+        k, k, p, p, p, pd, k, k,
+        k, p, p, p, p, p, pd, k,
+        p, p, w, k, p, w, k, pd,
+        p, p, w, w, p, w, w, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, k, p, p, k, p, pd, pd
+    ],
+    [
+        k, k, p, p, p, pd, k, k,
+        k, p, p, p, p, p, pd, k,
+        p, p, w, k, p, w, k, pd,
+        p, p, w, w, p, w, w, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, k, p, p, p, k, pd, pd
+    ],
+    [
+        k, k, p, p, p, pd, k, k,
+        k, p, p, p, p, p, pd, k,
+        p, p, k, w, p, k, w, pd,
+        p, p, w, w, p, w, w, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, k, p, p, p, k, pd
+    ],
+    [
+        k, k, p, p, p, pd, k, k,
+        k, p, p, p, p, p, pd, k,
+        p, p, k, w, p, k, w, pd,
+        p, p, w, w, p, w, w, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, p, p, p, p, pd, pd,
+        p, p, k, p, p, k, pd, pd
+    ],
+
+]
